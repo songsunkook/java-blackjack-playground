6c9f7a70e5db1972911bbe4562c25fb41bbaf90c
songsunkook <songsunkook@gmail.com>
2023-06-23 14:44:15 +0900

    feat: 카드 마크 enum

---
+++ src/main/java/constant/Mark.java
@@ -0,0 +1,19 @@
+package constant;
+
+public enum Mark {
+    DIAMOND("다이아몬드"),
+    SPADE("스페이드"),
+    HEART("하트"),
+    CLOVER("클로버"),
+    ;
+
+    private final String message;
+
+    Mark(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
