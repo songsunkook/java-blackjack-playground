0041f862b145b5b36c56a442e81ba8f71ceb55a8
songsunkook <songsunkook@gmail.com>
2023-06-23 11:28:46 +0900

    feat: Optional 요구사항 1

--- src/main/java/nextstep/optional/User.java
+++ src/main/java/nextstep/optional/User.java
@@ -1,5 +1,7 @@
 package nextstep.optional;
 
+import java.util.Optional;
+
 public class User {
     private String name;
     private Integer age;
@@ -33,7 +35,10 @@ public class User {
     }
 
     public static boolean ageIsInRange2(User user) {
-        return false;
+        return Optional.ofNullable(user)
+                .map(userElement -> userElement.age)
+                .filter(ageElement -> (ageElement >= 30 && ageElement <= 45))
+                .isPresent();
     }
 
     @Override
