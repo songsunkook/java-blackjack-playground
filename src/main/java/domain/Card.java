c415561952254ffcda0f4802ecdbc62330608248
songsunkook <songsunkook@gmail.com>
2023-06-23 21:47:30 +0900

    feat: 카드 숫자 출력

--- src/main/java/domain/Card.java
+++ src/main/java/domain/Card.java
@@ -1,6 +1,10 @@
 package domain;
 
 import constant.card.Mark;
+import constant.card.Number;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Card {
     private final int number;
@@ -12,6 +16,19 @@ public class Card {
     }
 
     public String getCardName() {
-        
+        return getNumber() + mark.name();
+    }
+
+    private String getNumber() {
+        if (number == 1 || number > 10) {
+            char result = Stream.of(Number.values())
+                    .filter(numberElement -> numberElement.getNumber() == number)
+                    .findFirst()
+                    .get()
+                    .toString()
+                    .charAt(0);
+            return Character.toString(result);
+        }
+        return Integer.toString(number);
     }
 }
