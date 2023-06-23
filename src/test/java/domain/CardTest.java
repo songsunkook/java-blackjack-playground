be3e831eb162a07661316a20bfed01d4f3a679d5
songsunkook <songsunkook@gmail.com>
2023-06-23 14:35:27 +0900

    test: 카드 정보 조회

---
+++ src/test/java/domain/CardTest.java
@@ -0,0 +1,33 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import javax.smartcardio.Card;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DisplayName("카드 ")
+public class CardTest {
+    @ParameterizedTest
+    @DisplayName("이름을 정확하게 불러오는가")
+    @MethodSource("getCardNameArguments")
+    public void getCardNameTest(int cardNumber, String cardMark, String displayName) {
+        assertThat(Card(cardNumber, cardMark).getCardName(), displayName);
+    }
+
+    private Stream<Arguments> getCardNameArguments() {
+        return Stream.of(
+                Arguments.of(1, Mark.DIAMOND, "A다이아몬드"),
+                Arguments.of(7, Mark.SPADE, "7스페이드"),
+                Arguments.of(10, Mark.HEART, "10하트"),
+                Arguments.of(13, Mark.CLOVER, "K클로버")
+        ).map(arguments -> Arguments.of(arguments.get()[0], arguments.get()[1], arguments.get()[2]));
+    }
+}
