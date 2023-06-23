ff9e6b5276545c2be12bddcdda2600cef14fa6a7
songsunkook <songsunkook@gmail.com>
2023-06-23 15:24:56 +0900

    feat: Card 클래스 생성

---
+++ src/main/java/domain/Card.java
@@ -0,0 +1,17 @@
+package domain;
+
+import constant.card.Mark;
+
+public class Card {
+    private final int number;
+    private final Mark mark;
+
+    public Card(int number, Mark mark) {
+        this.number = number;
+        this.mark = mark;
+    }
+
+    public String getCardName() {
+        
+    }
+}
