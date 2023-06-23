70f61e4e78f2ff240fe77c35057ee30c11bffbe4
songsunkook <songsunkook@gmail.com>
2023-06-23 16:14:37 +0900

    fix: 테스트 코드 수정

--- src/test/java/domain/CardTest.java
+++ src/test/java/domain/CardTest.java
@@ -16,15 +16,15 @@ public class CardTest {
     @DisplayName("이름을 정확하게 불러오는가")
     @MethodSource("getCardNameArguments")
     public void getCardNameTest(int cardNumber, Mark cardMark, String displayName) {
-        assertEquals(new Card(cardNumber, cardMark).getCardName(), displayName);
+        assertEquals(displayName, new Card(cardNumber, cardMark).getCardName());
     }
 
-    private Stream<Arguments> getCardNameArguments() {
+    private static Stream<Arguments> getCardNameArguments() {
         return Stream.of(
                 Arguments.of(1, Mark.DIAMOND, "A다이아몬드"),
                 Arguments.of(7, Mark.SPADE, "7스페이드"),
                 Arguments.of(10, Mark.HEART, "10하트"),
                 Arguments.of(13, Mark.CLOVER, "K클로버")
-        ).map(arguments -> Arguments.of(arguments.get()[0], arguments.get()[1], arguments.get()[2]));
+        );
     }
 }
