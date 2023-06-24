22ef2d6dee1de6f2684f858978041addaa5703a0
songsunkook <songsunkook@gmail.com>
2023-06-24 17:07:30 +0900

    feat: 클래스 생성

---
+++ src/main/java/domain/Deck.java
@@ -0,0 +1,9 @@
+package domain;
+
+import java.util.List;
+
+public class Deck {
+    List<Card> cards;
+
+
+}
---
+++ src/main/java/domain/Person.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Person {
+    Name name;
+    Deck deck;
+
+    public void addCard(Card card) {
+
+    }
+
+    public String getCardInfo(int index) {
+        return null;
+    }
+}
