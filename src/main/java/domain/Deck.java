b5ad3ee5786c10147bab55d7958622746f0003d3
songsunkook <songsunkook@gmail.com>
2023-06-24 18:37:03 +0900

    feat: 랜덤 카드 뽑기 구현

--- src/main/java/domain/Deck.java
+++ src/main/java/domain/Deck.java
@@ -1,7 +1,11 @@
 package domain;
 
+import constant.card.Mark;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 public class Deck {
     private final List<Card> cards = new ArrayList<>();
@@ -12,5 +16,18 @@ public class Deck {
 
     public String getCardInfo(int index) {
         return cards.get(index).getCardName();
+    }
+
+    public void drawCard(int amount) {
+        IntStream.range(0, amount).forEach(i -> addCard(randomCard()));
+    }
+
+    private Card randomCard() {
+        Random random = new Random();
+
+        int cardIndex = random.nextInt(13) + 1;
+        Mark cardMark = Mark.values()[random.nextInt(Mark.values().length)];
+
+        return new Card(cardIndex, cardMark);
     }
 }
